e3f5c08b45e4feb641594bb29866ab1e65d61171
Jonathan Lo <jonathanlo411@gmail.com>
2023-05-09 23:03:22 -0700

    feat: add get and post req samples

--- backend/1/routes/index.js
+++ backend/1/routes/index.js
@@ -3,7 +3,39 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  // Send Response
+  responseData = {
+    success: 1,
+    data: {
+      temp: "val"
+    }
+  }
+  res.json(responseData);
 });
+
+router.post('/', (req, res) => {
+  if (req.query.key && req.query.key == 4321) {
+    // Fake storage
+    let temp_obj = {
+      data: req.query.data
+    }
+    console.log(temp_obj)
+
+    // Send Response
+    const responseData = {
+      success: 1,
+      message: "Post Successful",
+    }
+    res.json(responseData)
+  } else {
+    // Send Failure
+    const responseData = {
+      success: 0,
+      message: "Post Unsuccessful",
+    }
+    res.json(responseData)
+  }
+})
 
 module.exports = router;
